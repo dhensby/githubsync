c766d08625d2a5eeec6db9e7021572c13833150b
Dan Hensby <git@hens.by>
2018-08-06 19:36:07 +0200

    Fix line endings of bin file

--- bin/githubsync.php
+++ bin/githubsync.php
@@ -1,1 +1,18 @@
-#!/usr/bin/env php<?phperror_reporting(E_ALL);foreach(array(    'vendor/autoload.php',    '../vendor/autoload.php',    '../../vendor/autoload.php',) as $dir) {    if (file_exists($dir)) {        require_once $dir;    }}$application = new \Dhensby\GitHubSync\Console\Application();$application->run();
+#!/usr/bin/env php
+<?php
+
+error_reporting(E_ALL);
+
+foreach(array(
+    'vendor/autoload.php',
+    '../vendor/autoload.php',
+    '../../vendor/autoload.php',
+) as $dir) {
+    if (file_exists($dir)) {
+        require_once $dir;
+    }
+}
+
+$application = new \Dhensby\GitHubSync\Console\Application();
+
+$application->run();
