b362b5982f4ae7b5e3996cb96c5bad0c136cf8e6
Dan Hensby <git@hens.by>
2018-08-09 23:59:04 +0100

    NEW Better comparison of branch commits and decide whether to rewind or force update

--- README.md
+++ README.md
@@ -15,5 +15,6 @@ Sync your forked repositories with their upstream parents.
 | Option | Behaviour |
 | --- | --- |
 | `--add-missing` | Add any branches present in the upstream repo that aren't present in the fork |
-| `--force` | Force any updates to branches when the change cannot be fast-forwarded |
+| `--force` | Force any updates to branches when the branches have diverged |
+| `--rewind` | Force any updates to branches when the change is a rewind |
 | `--from-source` | Update from the root source repo rather than the parent; this only applies if your repo forked a fork of an upstream and will cause the root repo to be used as the source of commits |
--- src/Console/Command/Repository/UpdateCommand.php
+++ src/Console/Command/Repository/UpdateCommand.php
@@ -18,7 +18,8 @@ class UpdateCommand extends Repository
             ->setName('repository:update')
             ->setDescription('Update your forked repository from a parent repo')
             ->addOption('add-missing', 'm', InputOption::VALUE_NONE, 'Add branches that are in the parent but missing from the origin')
-            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force updates of branches')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Force updates of branches when they have diverged (this will remove any custom changes pushed to the branch')
+            ->addOption('rewind', 'r', InputOption::VALUE_NONE, 'Allow rewinding a branch')
             ->addOption('from-source', 's', InputOption::VALUE_NONE, 'Update from source repo instead of parent');
     }
 
@@ -52,27 +53,73 @@ class UpdateCommand extends Repository
                         ]);
                         continue;
                     }
+                    $force = false;
                     $repoBranch = $repoBranches[$parentBranch['name']];
                     if ($parentBranch['commit']['sha'] !== $repoBranch['commit']['sha']) {
-                        $output->writeln(sprintf(
-                            '  - Updating branch %s (%s...%s)',
-                            $repoBranch['name'],
-                            substr($repoBranch['commit']['sha'], 0, 6),
-                            substr($parentBranch['commit']['sha'], 0, 6)
-                        ));
-                        try {
-                            $client->api('git')->references()->update($this->getOrganisation(), $repo['name'], 'heads/' . $repoBranch['name'], [
-                                'sha' => $parentBranch['commit']['sha'],
-                                'force' => $input->getOption('force'),
-                            ]);
-                        } catch (RuntimeException $e) {
-                            $extraMessage = '';
-                            // not found can be a generic "permission denied" error
-                            if ($e->getMessage() === 'Not Found') {
-                                $extraMessage = 'Make sure you are correcly authenticated with an access token with the public_repo permission';
+                        $comparedCommits = $client->api('repos')->commits()->compare(
+                            $this->getOrganisation(),
+                            $repo['name'],
+                            $parentBranch['commit']['sha'],
+                            $repoBranch['commit']['sha']
+                        );
+                        if (
+                            $comparedCommits['status'] == 'behind' ||
+                            ($force = $comparedCommits['status'] == 'diverged' && $this->getForce()) ||
+                            ($force = $comparedCommits['status'] == 'ahead' && $this->getRewind())
+                        ) {
+                            $message = sprintf(
+                                '  - %s branch %s (%s...%s)',
+                                $comparedCommits['status'] == 'ahead' ? 'Rewinding' : 'Updating',
+                                $repoBranch['name'],
+                                substr($repoBranch['commit']['sha'], 0, 9),
+                                substr($parentBranch['commit']['sha'], 0, 9)
+                            );
+                            if ($force) {
+                                $message .= ' (forced update)';
                             }
-                            // not a fast forward
-                            $output->writeln('    <error>' . $e->getMessage() . ' ' . $extraMessage . '</error>');
+                            $output->writeln($message);
+                            try {
+                                $client->api('git')->references()->update($this->getOrganisation(), $repo['name'], 'heads/' . $repoBranch['name'], [
+                                    'sha' => $parentBranch['commit']['sha'],
+                                    'force' => $force,
+                                ]);
+                            } catch (RuntimeException $e) {
+                                $extraMessage = '';
+                                // not found can be a generic "permission denied" error
+                                if ($e->getMessage() === 'Not Found') {
+                                    $extraMessage = 'Make sure you are correcly authenticated with an access token with the public_repo permission';
+                                }
+                                // not a fast forward
+                                $output->writeln('    <error>' . $e->getMessage() . ' ' . $extraMessage . '</error>');
+                            }
+                        } else {
+                            $output->writeln(sprintf(
+                                '  - Skipping branch %s because branch %s %s',
+                                $repoBranch['name'],
+                                $comparedCommits['status'] == 'diverged' ? 'has' : 'is',
+                                $comparedCommits['status']
+                            ));
+                            if ($output->isVerbose()) {
+                                $message = '';
+                                if ($comparedCommits['ahead_by']) {
+                                    $message .= sprintf(
+                                        '%d commit%s ahead',
+                                        $comparedCommits['ahead_by'],
+                                        $comparedCommits['ahead_by'] == 1 ? '' : 's'
+                                    );
+                                }
+                                if ($comparedCommits['behind_by']) {
+                                    if ($message) {
+                                        $message .= ', ';
+                                    }
+                                    $message .= sprintf(
+                                        '%d commit%s behind',
+                                        $comparedCommits['behind_by'],
+                                        $comparedCommits['behind_by'] == 1 ? '' : 's'
+                                    );
+                                }
+                                $output->writeln('    ' . $message);
+                            }
                         }
                     }
                 }
@@ -83,5 +130,15 @@ class UpdateCommand extends Repository
     public function getOnlyForks()
     {
         return true;
+    }
+
+    public function getForce()
+    {
+        return $this->getInput()->getOption('force');
+    }
+
+    public function getRewind()
+    {
+        return $this->getInput()->getOption('rewind');
     }
 }
