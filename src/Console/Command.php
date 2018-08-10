101698362235331366293d005061863190207b72
Dan Hensby <git@hens.by>
2018-08-10 14:00:40 +0100

    NEW Add caching to API responses

--- src/Console/Command.php
+++ src/Console/Command.php
@@ -2,7 +2,11 @@
 
 namespace Dhensby\GitHubSync\Console;
 
+use Cache\Adapter\Filesystem\FilesystemCachePool;
 use Github\Client;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Exception;
+use League\Flysystem\Filesystem;
 use Symfony\Component\Console\Command\Command as BaseCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -62,13 +66,32 @@ class Command extends BaseCommand
         return $this->output;
     }
 
+    /**
+     * @return Client
+     */
     public function getClient()
     {
         $client = new Client();
+        try {
+            $client->addCache($this->getCache());
+        } catch (Exception $e) {
+            $this->getOutput()->writeln('<error>Unable to create cache pool</error>');
+        }
         $token = exec('composer config -g github-oauth.github.com');
         if ($token) {
             $client->authenticate($token, Client::AUTH_HTTP_TOKEN);
         }
         return $client;
+    }
+
+    /**
+     * @return FilesystemCachePool
+     */
+    public function getCache()
+    {
+        $homePath = getenv('HOME') ?: getenv('USERPROFILE');
+        $fsAdapter = new Local($homePath . '/.config/githubsync/');
+        $fs = new Filesystem($fsAdapter);
+        return new FilesystemCachePool($fs);
     }
 }
