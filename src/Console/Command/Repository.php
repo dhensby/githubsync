a9fd28a31ef94711de191bd586e339c479fd35b8
Daniel Hensby <git@hens.by>
2018-08-20 14:40:59 +0100

    Nicer error if organisation is not found

--- src/Console/Command/Repository.php
+++ src/Console/Command/Repository.php
@@ -3,6 +3,7 @@
 namespace Dhensby\GitHubSync\Console\Command;
 
 use Dhensby\GitHubSync\Console\Command;
+use Github\Exception\RuntimeException;
 use Github\ResultPager;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
@@ -24,7 +25,15 @@ class Repository extends Command
     {
         $client = $this->getClient();
         $pager = new ResultPager($client);
-        $repos = $pager->fetchAll($client->api('user'), 'repositories', [$this->getOrganisation()]);
+        try {
+            $repos = $pager->fetchAll($client->api('user'), 'repositories', [$this->getOrganisation()]);
+        } catch (RuntimeException $e) {
+            $this->getOutput()->writeln(sprintf(
+                '<error>Unable to find repositories for %s, please make sure they exist or you have permission to access them</error>',
+                $this->getOrganisation()
+            ));
+            exit(1);
+        }
         $onlyForks = $this->getOnlyForks();
         return array_filter($repos, function ($repo) use ($onlyForks) {
             $whitelistRepos = $this->getInput()->getArgument('repository');
